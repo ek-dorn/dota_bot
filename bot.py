e948291a02466fd333e733d801b3483568f38ec8
pseusys <shveitsar215@gmail.com>
2024-02-07 20:32:15 +0100

    mentions fixed

--- bot.py
+++ bot.py
@@ -23,13 +23,16 @@ keywords = ["dota", "дота", "дотка", "доту", "дотку"]
 # Token
 token = environ["TELEGRAM_BOT_TOKEN"]
 
+# Mentions string
+mentions = "[гиперсодомит](tg://user?id=nkognit0), [мегагей](tg://user?id=madmaniako), [ультрапидор](tg://user?id=pseusys), [кибергомик](tg://user?id=bunnynobugs)"
+
 
 # Handle incoming messages
 async def keyword_response(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     message = update.message.text.lower()  # Get the text of the incoming message in lowercase
     for keyword in keywords:
         if keyword in message:
-            await update.message.reply_text(f"{keyword} [@nkognit0](гиперсодомит) [@madmaniako](мегагей), [@pseusys](ультрапидор), [@bunnynobugs](кибергомик)", parse_mode=ParseMode.MARKDOWN_V2)
+            await update.message.reply_text(f"{keyword} {mentions}", parse_mode=ParseMode.MARKDOWN)
             return
 
 
