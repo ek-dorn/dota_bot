dd7b3725d7531f5ebcac4bf20fc40c7de226cc9c
pseusys <shveitsar215@gmail.com>
2024-02-07 20:56:13 +0100

    semicolon added

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def keyword_response(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None
     message = update.message.text.lower()  # Get the text of the incoming message in lowercase
     for keyword in keywords:
         if keyword in message:
-            await update.message.reply_text(f"{keyword} {mentions}", parse_mode=ParseMode.MARKDOWN_V2)
+            await update.message.reply_text(f"{keyword}: {mentions}", parse_mode=ParseMode.MARKDOWN_V2)
             return
 
 
